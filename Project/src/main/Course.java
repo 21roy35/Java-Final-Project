9fab6646f929f8206c9730991011fa454ebe5d69
mansour <fancyalureon@gmail.com>
2022-04-24 00:45:31 +0300

    added Department class, added methods and constructor for Course class

--- Project/src/main/Course.java
+++ Project/src/main/Course.java
@@ -1,5 +1,52 @@
-package main;
+ package main;
+
+import java.util.ArrayList;
 
 public class Course {
+		 private String name;
+		 private int credits;
+		 private ArrayList<Course> prerequisites;
+		 private ArrayList<Section> sections;
+		 private ArrayList<Professor> professors;
 
-}
+		 public Course(String course, int credit, ArrayList<Course> prereqList, ArrayList<Section> sections, ArrayList<Professor> professors) {
+			 this.name=course;
+			 this.credits=credit;
+			 this.prerequisites=prereqList;
+			 this.sections=sections;
+			 this.professors=professors;
+		 }
+		 public void setName(String name) {
+		 	this.name=name;
+		 }
+		 public String getName() {
+		 	return this.name;
+		 }
+		 public void setCredits(int c) {
+		 	if (c>0) {
+		 		this.credits=c;
+		 }
+		 }
+		 public int getCredits() {
+		 	return this.credits;
+		 }
+		 public void setPrerequisites(ArrayList<Course> c_list) {
+		 	
+		 }
+		 public ArrayList<Course> getPrerequisites() {
+		 	return this.prerequisites;
+		 }
+		 public void createSections(Department d) {
+		 	//
+		 }
+		 public void removeSections(Department d) {
+		 	
+		 }
+		 public void setProfessors(ArrayList<Professor> professors) {
+		 	
+		 }
+		 public ArrayList<Professor> getProfessors(){
+			return this.professors;
+		 	
+		 }
+		 }
---
+++ Project/src/main/Department.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Department {
+
+	public Department() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
