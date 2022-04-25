a88c6f8e7db2f48c017883f580662eb4f1447ae2
user <user@LAPTOP-P1RTLURA>
2022-04-26 01:25:02 +0300

    added methods in Department class

---
+++ Project/src/Department.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Department {
+    String name;
+    ArrayList<Professor>professor= new ArrayList <Professor>();
+    ArrayList <Student> Student =new ArrayList <Student>();
+    
+    
+}
--- Project/src/main/Department.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Department {
-
-	public Department() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
