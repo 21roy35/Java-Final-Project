b5120ff70f8a8f1dd765defc440bca8c6f363d9d
Ahmed Alsaggaf <50363709+ahm-sag@users.noreply.github.com>
2022-05-01 01:13:37 +0300

    Delete Department.java

--- Project/src/Department.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Department {
-    String name;
-    ArrayList<Professor>professor= new ArrayList <Professor>();
-    ArrayList <Student> Student =new ArrayList <Student>();
-    
-    
-}
