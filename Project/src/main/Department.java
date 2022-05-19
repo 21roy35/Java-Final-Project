49e05e3551c64b84a488b4003fd87b33ac1cc91c
ighxh19 <aalghamdi3621@stu.kau.edu.sa>
2022-05-19 22:51:24 +0300

    edited Department class

--- Project/src/main/Department.java
+++ Project/src/main/Department.java
@@ -1,11 +1,66 @@
-package main;
-
 import java.util.ArrayList;
 
+import main.*;
+
 public class Department {
-    String name;
-    ArrayList<Professor>professor= new ArrayList <Professor>();
-    ArrayList <Student> Student =new ArrayList <Student>();
-    
-    
+
+    private String name;
+    private Major major;
+    private  ArrayList<Professor>professor= new ArrayList <Professor>();
+    private ArrayList <Student> Student =new ArrayList <Student>();
+
+
+    public Department (String name , Major major , ArrayList<Professor> professors, ArrayList<Student> students){
+        this.name=name;
+        this.major=major;
+        for (int i=0; i<professor.size(); i++) {
+            professor.add(professors.get(i));
+        }
+        this.Student=students;
+    }
+
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public String  getName () {
+        return name;
+    }
+
+    public void addProfessorList (ArrayList<Professor>professors) {
+        for (int i=0; i<professor.size(); i++) {
+            professor.add(professors.get(i));
+        }
+    }
+
+
+    public void removeProfessorList(ArrayList<Professor> professors) {
+
+        for (int i=0; i<professor.size(); i++) {
+            professor.remove(professors.get(i));
+        }
+    }
+
+    public ArrayList<Professor> getProfessorList(){
+        return this.professor;
+    }
+
+    public void addStudentList (ArrayList<Student> students) {
+        for (int i=0; i<students.size(); i++) {
+            students.add(students.get(i));
+        }
+
+    }
+    public void removeStudentList(ArrayList<Student> students) {
+        for (int i=0; i<students.size(); i++) {
+            students.remove(students.get(i));
+        }
+
+    }
+
+    public ArrayList<Student> getStudentList (){
+        return this.Student;
+    }
+
+
 }
